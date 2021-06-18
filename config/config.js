826bfd2318c4a0084389916e2e71399258a57d86
Adarsh <adarsh.bhautoo@gmail.com>
2021-06-18 14:02:57 +0400

    [Add Config] - Use Config instead of hard coded

---
+++ config/config.js
@@ -0,0 +1,37 @@
+let config = {};
+config.hubs=[
+	{domain:'uat.kyowakirinhub.com', hubId:'hub-id'},
+	{domain:'kki-hub-dev.rapp-customers.co.uk', hubId:'hub-id'},
+	{domain:'kki-hub.rapp-customers.co.uk', hubId:'hub-id'},
+	{domain:'hww.kkihub.s3-website.us-east-2.amazonaws.com', hubId:'37dd7bf6-5628-4aac-8464-f4894ddfb8c4'},
+	{domain:'kyowakirinhub.com', hubId:'hub-id'}
+]
+
+
+
+config.contentTypes=['Layout H - Content promo','Layout C - Main therapy page','Layout E - Article' ,
+'Layout D - Content list page','Layout D - Content list page','Layout G - Main Promo Page','Layout K - Generic page',
+'Layout - Veeva player page','Layout J - Main Promo Page Alternate','Layout - Content page','Layout - Pre-registration page',
+'Layout E - Dynamic article','Layout F - Dynamic content page','eDetailer Page'];
+
+
+config.libraries = [
+  { code: 'de-de',libraryId: '040d342f-c78d-44c2-872c-b5f0acd07d6b'},
+  { code: 'en-ie',libraryId: '4838a63c-9cd1-4d4e-a28e-3db6fda6ac38'},
+  { code: 'fr-fr',libraryId: '95cc85ac-1669-4088-bcd2-c8fff18bd617'},
+  { code: 'it-it',libraryId: 'dd57ecba-c892-4303-b61a-ae43c17c7fbf'},
+  { code: 'nl-nl',libraryId: '0a861b00-ad92-4d19-b851-443f11c4a683'},
+  { code: 'no-no',libraryId: 'c8d215b7-2b92-42c7-b8bf-22ce07727798'},
+  { code: 'en-gb',libraryId: '77d05f48-8ade-4160-be16-09a97605c322'},
+  { code: 'es-es',libraryId: '064d499c-fad8-46bf-96d9-99a1c323743f'},
+  { code: 'en-sa',libraryId: 'ba7ed98d-e84f-40c7-abd9-a0614f400766'}
+];
+
+config.baseQuery='https://content-eu-1.content-cms.com/api/#hubId#/delivery/v1/';
+config.searchQuery='search';
+config.getItemById='context/';
+config.queryMain ='?q=*:*&fl=name,string1,id,classification,type,status&fq=type:(#contenttypes#)&rows=10000&start=0&fq=libraryId:(#libraryid#)';
+
+
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const axios = require('axios');
+var config = require('./config/config');
 
 async function getItemDetail(domain,path,culture)
 {
-    
+   
+    let hubId=getHubId(domain);
+    let libraryId=getLibraryId(culture);
+    let contentTypeQuery = getContentTypes(); 
+    let routesQuery= buildRoutesQuery(hubId,libraryId,contentTypeQuery);    
+
     let xframeOption = "allow";
-      //hardcode domain - to non prod 8 for Uk site
-    let itemRoutesUrl = "https://content-eu-1.content-cms.com/api/37dd7bf6-5628-4aac-8464-f4894ddfb8c4/delivery/v1/search?q=*%3A*";
-    itemRoutesUrl=itemRoutesUrl+"&fl=name%2Cstring1%2Cid%2Cclassification%2Ctype%2Cstatus&fq=type%3A%28%22Layout+H+-+Content+promo%22+OR+%22Layout";
-    itemRoutesUrl=itemRoutesUrl +"+C+-+Main+therapy+page%22+OR+%22Layout+E+-+Article%22+OR+%22Layout+D+-+Content+list+page%22+";
-    itemRoutesUrl=itemRoutesUrl+"OR+%22Layout+G+-+Main+Promo+Page%22+OR+%22Layout+K+-+Generic+page%22+OR+%22Layout+-";
-    itemRoutesUrl=itemRoutesUrl +"+Veeva+player+page%22+OR+%22Layout+J+-+Main+Promo+Page+Alternate%22+OR+%22Layout+-+Content+page%22+OR+%22Layout+-";
-    itemRoutesUrl=itemRoutesUrl  +"+Pre-registration+page%22+OR+%22Layout+E+-+Dynamic+article%22+OR+%22Layout+F+-+Dynamic+content+page%22+OR";
-    itemRoutesUrl=itemRoutesUrl  +"+%22eDetailer+Page%22%29&rows=10000&start=0&fq=libraryId%3A%28%2277d05f48-8ade-4160-be16-09a97605c322%22%29";
-    
-    let result = await axios.get(itemRoutesUrl).then((response) => {
+
+    let result = await axios.get(routesQuery).then((response) => {
         let items = response.data.documents;
         let numFound = response.data.numFound;
         if(numFound > 0)
@@ -40,8 +38,8 @@ async function getItemDetail(domain,path,culture)
     if(result.code =="ok")
     {
         let id=result.data;
-        let itemDataPath = "https://content-eu-1.content-cms.com/api/37dd7bf6-5628-4aac-8464-f4894ddfb8c4/delivery/v1/rendering/context/"+id;
-        let itemData = await axios.get(itemDataPath).then((response) => {
+        let itemQuery = buildItemQuery(hubId, id);
+        let itemData = await axios.get(itemQuery).then((response) => {
                 return response.data.elements.xFrameOption.value;
         }).catch((error) => {
               console.error('api request error1', error);
@@ -55,24 +53,97 @@ async function getItemDetail(domain,path,culture)
     
 }
 
+function getHubId(domain)
+{
+    let hubs = config.hubs;
+    let hubId='';
+    if(hubs.length > 0)
+    {
+        for(let i=0;i< hubs.length;i++)
+        {
+            if(domain == hubs[i].domain)
+            {
+                return hubs[i].hubId;
+                
+            }
+        }        
+    }
+    return '';
+}
+
+function getLibraryId(culture)
+{ 
+    let libraries = config.libraries;   
+    if(libraries.length > 0)
+    {
+        for(let i=0;i< libraries.length;i++)
+        {
+            if(libraries[i].code == culture)
+            {
+                return libraries[i].libraryId;
+            }
+        }        
+    }
+
+    return '';
+}
+
+function getContentTypes()
+{
+    let contentTypes=config.contentTypes;
+    let contentTypeQuery='';
+
+    if(contentTypes.length> 0)
+    {
+        for(let i=0;i< contentTypes.length;i++)
+        {
+            if(contentTypeQuery.length == 0)
+            {
+                contentTypeQuery='"'+contentTypes[i]+'"';
+            }
+            else
+            {
+                contentTypeQuery = contentTypeQuery+' OR "'+contentTypes[i]+ '"';
+            }
+        }
+    }
+    return contentTypeQuery;
+}
+
+function buildRoutesQuery(hubId,libraryId,contentTypes)
+{
+    let baseQuery=config.baseQuery.replace('#hubId#',hubId);
+    let searchQuery=config.searchQuery;
+    let mainQuery = config.queryMain;
+    mainQuery=mainQuery.replace('#contenttypes#',contentTypes).replace('#libraryid#',libraryId);
+
+    let finalQuery=baseQuery+searchQuery+mainQuery;
+    return finalQuery;
+}
+
+function buildItemQuery(hubId,contentId)
+{
+    let baseQuery=config.baseQuery.replace('#hubId#',hubId);
+    let itemQuery=config.getItemById + contentId;
+    let finalQuery=baseQuery+itemQuery;
+    return finalQuery;
+
+}
+
 exports.handler = async (event, context, callback) => {
-    /*
-     * Generate HTTP response using 200 status code with a simple body.
-     */
-     
+    
     let path = event.Records[0].cf.request.uri;
     let domain = event.Records[0].cf.request.origin.custom.domainName;
     let itempath = path.substring(6); 
-    let culture = path.substring(1,6); 
-    let xframeOption = await getItemDetail(domain,itempath,culture);
-   
+    let culture = path.substring(1,6);    
+    
+    let xframeOption = await getItemDetail(hubId,itempath,culture);
+
     const response = event.Records[0].cf.response;
     const headers = response.headers;
    
-   let xssP = '1; mode=block;path='+path + ' domain='+domain +' culture='+culture;
-
     headers['x-frame-options'] = [{key: 'X-Frame-Options', value: xframeOption}]; 
-    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: xssP}]; 
+    headers['x-xss-protection'] = [{key: 'X-XSS-Protection', value: '1; mode=block;'}]; 
     headers['referrer-policy'] = [{key: 'Referrer-Policy', value: 'same-origin'}]; 
     
     //Return modified response
